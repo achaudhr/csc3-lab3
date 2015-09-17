629ce769fd679124ff5746eef260f3d225cd4eca
Mona Chaudhry <Mona Chaudhry@MonaCh.MonaCh>
2015-09-17 17:25:24 -0400

    modularized and working

--- ConfigrableSort/src/ConfigReader.java
+++ ConfigrableSort/src/ConfigReader.java
@@ -17,103 +17,138 @@ public class ConfigReader {
 	
 	static ArrayList<Integer> myArrayList =new ArrayList<>();
 
-	public static void main(String argv[]) {
-
+	
+	public static ArrayList<Integer> parseConfigFile()
+	{
 		File fXmlFile = new File("myconfig.xml");
 		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
 		DocumentBuilder dBuilder;
-		try {
-			dBuilder = dbFactory.newDocumentBuilder();
 
-			Document doc = dBuilder.parse(fXmlFile);
+			try {
+				dBuilder = dbFactory.newDocumentBuilder();
+				Document doc = dBuilder.parse(fXmlFile);
+			NodeList algsElement=parseAlgorithmsElement(doc);
+			NodeList algElement=parseAlgorithmElement(algsElement);
+			NodeList inputElements=parseInputElement(algElement);
+			return parseEachDataItem(inputElements); 
+			} 
+			catch (ParserConfigurationException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (SAXException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 
-			// optional, but recommended
-			// read this -
-			// http://stackoverflow.com/questions/13786607/normalization-in-dom-parsing-with-java-how-does-it-work
-			doc.getDocumentElement().normalize();
+			
+			return null;
+		
+	}
+	
+	public static NodeList parseAlgorithmsElement(Document doc) 
+	{
+		doc.getDocumentElement().normalize();
 
-			System.out.println("Root element :"
-					+ doc.getDocumentElement().getNodeName());
+		System.out.println("Root element :"
+				+ doc.getDocumentElement().getNodeName());
 
-			NodeList nList = doc.getElementsByTagName("algorithms");
+		NodeList nList = doc.getElementsByTagName("algorithms");
+		System.out.println("----------------------------");
+		return nList;
+	}
+	
+	public static NodeList parseAlgorithmElement(NodeList nList)
+	{
+		NodeList inputList=null;
+		for (int temp = 0; temp < nList.getLength(); temp++) {
+			Node nNode = nList.item(temp);
+			System.out.println("\nCurrent Element :" + nNode.getNodeName());
 
-			System.out.println("----------------------------");
-
-			for (int temp = 0; temp < nList.getLength(); temp++) {
-				Node nNode = nList.item(temp);
-				System.out.println("\nCurrent Element :" + nNode.getNodeName());
-
-				if (nNode.getNodeType() == Node.ELEMENT_NODE) {
-					Element eElement = (Element) nNode;
-					NodeList inputList = nNode.getChildNodes();
-
-					if (inputList != null) {
-						for (int j = 0; j < inputList.getLength(); j++) {
-							Node eachNode = inputList.item(j);
-							if (eachNode.getNodeType() == Node.ELEMENT_NODE) {
-								Element algorithmElement = (Element) eachNode;
-								System.out
-										.println("Name : "
-												+ algorithmElement
-														.getAttribute("name"));
-								NodeList elementList = eachNode.getChildNodes();
-								if (elementList != null) {
-									for (int k = 0; k < elementList.getLength(); k++) {
-										Node elementNode = elementList.item(k);
-										 
-										if (elementNode.getNodeType() == Node.ELEMENT_NODE) {
-											NodeList list = elementNode.getChildNodes();
+			if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+				Element eElement = (Element) nNode;
+				 inputList = nNode.getChildNodes();
+				
+	}}
+		return inputList;
+		}
+			
+	public static NodeList 	parseInputElement (NodeList inputList)
+	{
+		NodeList elementList=null;
+		if (inputList != null) {
+			for (int j = 0; j < inputList.getLength(); j++) {
+				Node eachNode = inputList.item(j);
+				if (eachNode.getNodeType() == Node.ELEMENT_NODE) {
+					Element algorithmElement = (Element) eachNode;
+					System.out
+							.println("Name : "
+									+ algorithmElement
+											.getAttribute("name"));
+					elementList = eachNode.getChildNodes();
+				}
+	}
+			}
+		
+		return elementList;
+	}
+				
+	public static ArrayList<Integer> parseEachDataItem(NodeList elementList)
+	{
+		if (elementList != null) {
+			for (int k = 0; k < elementList.getLength(); k++) {
+				Node elementNode = elementList.item(k);
+				 
+				if (elementNode.getNodeType() == Node.ELEMENT_NODE) {
+					NodeList list = elementNode.getChildNodes();
+					
+					if (list != null) {
+						for (int m = 0; m < list.getLength(); m++) {
+							
+							Node inputNode =list.item(m);
+							
+							System.out
+							.println(inputNode.getNodeName());
+							
+							NodeList valueList = inputNode.getChildNodes();
+							
+							if (valueList != null) {
+								for (int n = 0; n < valueList.getLength(); n++) {
+									Node valNode = valueList.item(n);	
+									
+									System.out
+									.println(valNode.getNodeValue());		//this line has the array values
+										
+									myArrayList.add(new Integer(valNode.getNodeValue()));
+									
 											
-											if (list != null) {
-												for (int m = 0; m < list.getLength(); m++) {
-													
-													Node inputNode =list.item(m);
-													
-													System.out
-													.println(inputNode.getNodeName());
-													
-													NodeList valueList = inputNode.getChildNodes();
-													
-													if (valueList != null) {
-														for (int n = 0; n < valueList.getLength(); n++) {
-															Node valNode = valueList.item(n);	
-															
-															System.out
-															.println(valNode.getNodeValue());		//this line has the array values
-																
-															myArrayList.add(new Integer(valNode.getNodeValue()));
-															
-																	
-															
-														}
-													}
-													
-												}
-											}
-										}
-									}
- 								}
+									
+								}
 							}
+							
 						}
 					}
 				}
 			}
+			}
+		return myArrayList;
+	}
+	
 
-		} catch (ParserConfigurationException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (SAXException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
+	public static void main(String argv[]) {
+	
+		myArrayList=QuickSort.quickSort(parseConfigFile());
+		System.out.println(myArrayList);
+
+			
+						}
+					
+
+					
 		
 	
-		System.out.println(myArrayList.toString());
-		myArrayList=QuickSort.quickSort(myArrayList);
-		System.out.println(myArrayList);
+		
 	}
-}
+
